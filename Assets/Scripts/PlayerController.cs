d33e9949820ebbf8b972b093d9937b3bf12e2172
Felix Wahl <felix.wahl@live.de>
2022-10-14 18:45:07 +0200

    Added project manually

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    //private PlayerActions playerActions; 
+    //private Rigidbody rigidbody;
+    private Vector2 moveInput;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnJump()
+    {
+        
+    }
+}
