835769450ed39137d0e891eeac06c0e8ea16fb78
Felix Wahl <felix.wahl@live.de>
2023-04-12 13:36:15 +0200

    [ADDED] Expanded behavior tree

---
+++ Assets/_Scripts/BehaviorTree/Behaviors/SpitterBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using BehaviorTree.Nodes.Action;
+using BehaviorTree.Nodes.Composite;
+using UnityEngine;
+
+namespace BehaviorTree.Enemies
+{
+    public class SpitterBehavior : Tree
+    {
+        
+        //TODO: testing purpose. entities later.
+        [SerializeField] private new Transform transform;
+        
+        
+        protected override Node CreateTree()
+        {
+            var root = new Selector(new List<Node>
+            {
+                new Sequence(new List<Node>
+                {
+                    new IsTargetInRange(transform)
+                })
+            });
+            
+            return root;
+        }
+    }
+}
---
+++ Assets/_Scripts/BehaviorTree/Nodes/Action/IsTargetInRange.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace BehaviorTree.Nodes.Action
+{
+    public class IsTargetInRange : Node
+    {
+        [SerializeField] private string targetID;
+        
+        //TODO: Test purpose. Entity should be passed in from the tree.
+        [SerializeField] private float detectionRadius = 10f;
+        [SerializeField] private LayerMask layerMask = 0;
+        [SerializeField] private new Transform transform;
+        
+        public IsTargetInRange(Transform transform)
+        {
+            this.transform = transform;
+        }
+        
+        [ContextMenu("Generate guid for target id")]
+        private void GenerateGuid()
+        {
+            targetID = Guid.NewGuid().ToString();
+        }
+
+        public override ReturnStat Tick()
+        {
+            var summoner = GetData(targetID);
+            if (summoner == null)
+            {
+                var colliders = Physics.OverlapSphere(transform.position, detectionRadius, layerMask);
+                if(colliders.Length > 0)
+                {
+                    Parent.Parent.SetData(targetID, colliders[0].transform);
+                    Result = ReturnStat.SUCCESS;
+                    return Result;
+                }
+            
+                Result = ReturnStat.FAILURE;
+                return Result;
+            }
+            else
+            {
+                var colliders = Physics.OverlapSphere(transform.position, detectionRadius, layerMask);
+                if(colliders.Length == 0)
+                {
+                    Parent.Parent.ClearData(targetID);
+                    Result = ReturnStat.FAILURE;
+                    return Result;
+                }
+            
+                Result = ReturnStat.SUCCESS;
+                return Result;
+            }
+        }
+    }
+}
